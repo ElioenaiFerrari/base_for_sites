dd3eae7a9180c076168c1e5d54079f5d3a64edb7
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-10 20:29:52 -0300

    config

--- ssl/ssl.sh
+++ ssl/ssl.sh
@@ -9,3 +9,12 @@ then
   make ssl
   echo "building complete."
 fi
+
+APP_HOST=$(cat .env | grep APP_HOST | cut -d= -f2)
+search_host=$(cat /etc/resolv.conf | grep $APP_HOST)
+
+if [ -z $search_host ]
+then
+  echo "registering host..."
+  sed -i "/127.0.0.1/a127.0.0.1\t$APP_HOST" /etc/hosts
+fi
