963eedd38e1e97fb70c7e5750a4a0a047aee59a6
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-10 18:34:25 -0300

    config

--- ssl/ssl.sh
+++ ssl/ssl.sh
@@ -9,14 +9,3 @@ then
   make ssl
   echo "building complete."
 fi
-
-APP_HOST=$(cat .env | grep APP_HOST | cut -d= -f2)
-search_host=$(cat /etc/resolv.conf | grep $APP_HOST)
-
-if [ -z $search_host ]
-then
-  echo "registering host..."
-  echo "nameserver 127.0.0.1\\
-        search $APP_HOST\\
-        options ndots:2" >> /etc/resolv.conf
-fi
